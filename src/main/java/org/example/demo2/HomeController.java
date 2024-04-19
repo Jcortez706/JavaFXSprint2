579d82bb78ede6abf58fa1332c4a8a67bc74848b
Jacob Cortez <jacob.cortez@teamseco.com>
2024-04-19 15:45:54 -0400

    Added customer, navigation, place order button variables

--- src/main/java/org/example/demo2/HomeController.java
+++ src/main/java/org/example/demo2/HomeController.java
@@ -1,5 +1,13 @@
 package org.example.demo2;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
+
 public class HomeController {
 
+    public Button customerListButton;
+    public Button navigationButton;
+    public Button placeOrderButton;
 }
