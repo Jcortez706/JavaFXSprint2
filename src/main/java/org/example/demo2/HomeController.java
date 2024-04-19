7cfed5a5eac60940ea5e14cdc91a4a1c72a2fb0e
Jacob Cortez <jacob.cortez@teamseco.com>
2024-04-19 16:20:27 -0400

    Added calls to onClickMethods

--- src/main/java/org/example/demo2/HomeController.java
+++ src/main/java/org/example/demo2/HomeController.java
@@ -1,13 +1,40 @@
 package org.example.demo2;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DialogPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeController {
 
     public Button customerListButton;
     public Button navigationButton;
     public Button placeOrderButton;
+
+    public void onCustomerClicked(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) customerListButton.getScene().getWindow();
+        stage.close();
+        Stage homeStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("customer-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load());
+        stage.setTitle("ACME");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onOrderClicked(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) placeOrderButton.getScene().getWindow();
+        stage.close();
+        Stage homeStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("order-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load());
+        stage.setTitle("ACME");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
