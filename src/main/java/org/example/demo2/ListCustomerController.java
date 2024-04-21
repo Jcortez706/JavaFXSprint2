6190eaa7b43ffb709bfd5703f026db47d62772e1
Jacob Cortez <jacob.cortez@teamseco.com>
2024-04-21 11:31:45 -0400

    Loaded array of customers into customerListView

--- src/main/java/org/example/demo2/ListCustomerController.java
+++ src/main/java/org/example/demo2/ListCustomerController.java
@@ -1,15 +1,25 @@
 package org.example.demo2;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ListCustomerController {
+public class ListCustomerController implements Initializable {
+    @FXML
     public Button addCustomerButton;
+    @FXML
+    public ListView<String> customerListView;
+    String[] customer = {"SECO", "LOCAL"};
+    String currentCustomer;
 
     public void AddCustomerClicked(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) addCustomerButton.getScene().getWindow();
@@ -21,5 +31,11 @@ public class ListCustomerController {
         stage.setTitle("ACME");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        customerListView.getItems().addAll(customer);
+
     }
 }
