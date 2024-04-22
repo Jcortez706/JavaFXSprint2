04299629a91ceac58e20d4575b9af47c09757340
Jacob Cortez <jacob.cortez@teamseco.com>
2024-04-22 10:24:47 -0400

    List view auto populates existing customers for user to see

---
+++ src/main/java/org/example/demo2/ViewCustomerController.java
@@ -0,0 +1,27 @@
+package org.example.demo2;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewCustomerController implements Initializable {
+
+    public Label customerName;
+    public Label streetAddress;
+    public Label city;
+    public Label state;
+    public Label zip;
+    public Label beerLicense;
+    public Label payment;
+    public Label loadingDockCapabilities;
+    public Label deliveryConstraints;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+    
+    
+}
+
+}
